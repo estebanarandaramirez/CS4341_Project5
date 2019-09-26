e3c5e1ed5bac9bdd9cdfbd387f541c6a61596778
Treksh Marwaha <trekshcool@gmail.com>
2019-09-26 15:06:30 -0400

    backtracking skeleton

--- Project5.py
+++ Project5.py
@@ -7,6 +7,50 @@ def backtrack(variable, value, limits, unary_inclusive, unary_exclusive, binary_
 # Select the MRV variable to fill
 # Fill in a value and solve further (recursively),
 # backtracking an assignment when stuck
+# Finds 90% rounded down
+
+
+def maximum_capacity_helper(capacity):
+    return round(0.9*capacity)
+
+
+class CSP(object):
+    def __init__(self, items, bags):
+        self.bags = bags
+        self.items = items
+
+        for item_name in self.items:
+            self.items[item_name].possible_bags = self.bags.copy()
+
+class Item(object):
+    def __init__(self, name, weight):
+        # Name of the Item
+        self.name = name
+        # Weight of the item
+        self.weight = int(weight)
+        # The bag that item is in
+        self.bag = None
+        # Possible bags
+        self.possible_bags = {}
+        # Constraints of item
+        self.constraints = []
+
+    def __eq__(self, other):
+        if isinstance(other, Item):
+            return self.name == other.name
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+
+    def putInBag(self, bag):
+        if self.bag:
+            self.bag.items = [s for s in self.bag.items if s.name is not self.name]
+        bag.items.append(self)
+        self.bag = bag
 
 
 class Variable(NamedTuple):
